860ddd6d8999e0a966b68c527251979c8d6292dd
ASLIHAN AMBESKOVIC <aslihan_petenkaya@urmc.rochester.edu>
2018-04-26 12:08:25 -0400

    Bash file to use the perl script in a loop

---
+++ simulated_data_conversion_fa_to_fq/fastqconverter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for fn in simulated_reads1/fasta/*.fasta;
+do
+samp=`basename ${fn}`
+echo "${samp}"
+echo "${fn}"
+	perl /Users/apetenkaya/Desktop/fasta_to_fastq.pl "${fn}" > "simulated_reads1/logs/converted_${samp}.fq"
+done
