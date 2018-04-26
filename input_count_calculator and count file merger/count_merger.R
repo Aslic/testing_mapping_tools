3f3c09de070beb8a10485507cdff7cb8f5485d1b
ASLIHAN AMBESKOVIC <aslihan_petenkaya@urmc.rochester.edu>
2018-04-26 12:56:56 -0400

    creates a single data frame from multiple count files

---
+++ input_count_calculator and count file merger/count_merger.R
@@ -0,0 +1,20 @@
+setwd("/Users/apetenkaya/Desktop/kallisto/kallisto_ch22_bs100/")#give the directory where the read folders are stored
+dirs<-list.files()
+names<-vector()
+files<-list()
+for(i in 1:length(dirs)){
+  setwd(dirs[i])
+  print (getwd())
+  names[i]<-dirs[i]
+  files[[i]]<-read.table("abundance.tsv",header = TRUE,stringsAsFactors = FALSE,sep="\t")#give the file name
+  print(summary(files[[i]]))
+  setwd("../")
+}
+
+all_files<-do.call(cbind,files)
+read_mat<-all_files[,grepl("est_counts",names(all_files))] #give the header where the counts are
+read_mat<-cbind(all_files[,1],read_mat)
+names(read_mat)<-c("id",names)
+informative<-read_mat[rowSums(read_mat[2:ncol(read_mat)])!=0,]
+setwd("/Users/apetenkaya/Desktop/")
+write.csv(informative,"kallistobs_counts.csv")
