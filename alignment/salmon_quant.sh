db31aa995f6e473987b99e2a18288110c7597df9
Aslic <38363261+Aslic@users.noreply.github.com>
2019-05-31 10:39:16 -0400

    Update salmon_quant.sh

--- alignment/salmon_quant.sh
+++ alignment/salmon_quant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /Users/apetenkaya/anaconda2/pkgs/salmon-0.8.2-1/bin/
-for fn in /Users/apetenkaya/Desktop/input_fastq/logs/*;
+cd /salmon-0.8.2-1/bin/
+for fn in /input_fastq/logs/*;
 do
 filename=$(basename -- "$fn")
-salmon quant -i /Users/apetenkaya/Desktop/human_trans_index -l A \
+salmon quant -i /human_trans_index -l A \
 -r "${fn}" --fldMean 250 --fldSD 25 --writeUnmappedNames -p 8 \
--o "/Users/apetenkaya/Desktop/${filename}_quant"
+-o "/${filename}_quant"
 done
