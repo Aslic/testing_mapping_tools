fe0b3b33172a21d1d6dca4a886f82bdcf3133aa5
Aslic <38363261+Aslic@users.noreply.github.com>
2019-05-31 10:40:22 -0400

    Update star_genome_aligner.sh

--- alignment/star_genome_aligner.sh
+++ alignment/star_genome_aligner.sh
@@ -3,14 +3,14 @@
 #SBATCH -N 1
 #SBATCH --mem=80gb
 
-cd /scratch/apetenka/logs/
+cd /logs/
 module load rnastar
-for fn in /scratch/apetenka/logs/*.fq;
+for fn in /logs/*.fq;
 do
 filename=$(basename -- "$fn")
 STAR \
---runThreadN 20 --twopassMode Basic --runMode alignReads --genomeDir /scratch/apetenka/genomeref/ \
---sjdbGTFfile /scratch/apetenka/genomeref/Homo_sapiens.GRCh38.92.chr.gtf   --sjdbOverhang 100 \
+--runThreadN 20 --twopassMode Basic --runMode alignReads --genomeDir /genomeref/ \
+--sjdbGTFfile /genomeref/Homo_sapiens.GRCh38.92.chr.gtf   --sjdbOverhang 100 \
 --readFilesIn "${fn}" \
---outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical --outFileNamePrefix "/scratch/apetenka/genomeAligned/${filename}"
+--outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical --outFileNamePrefix "/genomeAligned/${filename}"
 done
