14a40b1c1304b32b36df61ef62d95ef87962506f
Aslic <38363261+Aslic@users.noreply.github.com>
2019-05-31 10:41:56 -0400

    Update genome_star_index.sh

--- indexing/genome_star_index.sh
+++ indexing/genome_star_index.sh
@@ -3,11 +3,11 @@
 #SBATCH -N 1
 #SBATCH --mem=80gb
 
-cd /scratch/apetenka/genomeref/
+cd /genomeref/
 module load rnastar
 
 STAR \
---runThreadN 32 --runMode genomeGenerate --genomeDir /scratch/apetenka/genomeref/ \
---genomeFastaFiles /scratch/apetenka/genomeref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+--runThreadN 32 --runMode genomeGenerate --genomeDir /genomeref/ \
+--genomeFastaFiles /genomeref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
 
