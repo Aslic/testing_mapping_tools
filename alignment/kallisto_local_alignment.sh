7a217e77f7f23640e9da116e7a8160dd79518a96
Aslic <38363261+Aslic@users.noreply.github.com>
2019-05-31 10:38:37 -0400

    Update kallisto_local_alignment.sh

--- alignment/kallisto_local_alignment.sh
+++ alignment/kallisto_local_alignment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for fn in /Users/apetenkaya/Desktop/simulated_reads1/logs/*;
+for fn in /logs/*;
 do
 	samp=`basename ${fn}`
-	kallisto quant -i /Users/apetenkaya/Desktop/kallisto/human_transcripts.idx -o "/Users/apetenkaya/Desktop/${samp}_kallisto" --single -b 100 -l 250 -s 25 "${fn}"
+	kallisto quant -i /kallisto/human_transcripts.idx -o "/Desktop/${samp}_kallisto" --single -b 100 -l 250 -s 25 "${fn}"
 done
 
