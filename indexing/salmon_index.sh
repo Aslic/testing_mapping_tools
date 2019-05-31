df08880cfa5f90d559db97aeb9218c6801f99fbd
Aslic <38363261+Aslic@users.noreply.github.com>
2019-05-31 10:42:33 -0400

    Update salmon_index.sh

--- indexing/salmon_index.sh
+++ indexing/salmon_index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /Users/apetenkaya/anaconda2/pkgs/salmon-0.8.2-1/bin/
-salmon index -t /Users/apetenkaya/Desktop/Homo_sapiens.GRCh38.cdna.all.fa -i /Users/apetenkaya/Desktop/human_trans_index
+cd /salmon-0.8.2-1/bin/
+salmon index -t Homo_sapiens.GRCh38.cdna.all.fa -i human_trans_index
